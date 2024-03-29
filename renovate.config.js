cc84f416e8a82212a4d86bedcb16ce49f413dfe2
HarishkumarSubramanian <116288939+HarishkumarSubramanian@users.noreply.github.com>
2024-03-29 10:31:41 +0530

    config file

---
+++ renovate.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    repositories: ['Harishkumar-Subramanian/test'],
+    customManagers: [
+      {
+        customType: 'regex',
+        fileMatch: ['values.yaml'],
+        matchStrings: ['(?<currentValue>helm:\\s*targetRevision:\\s*)(?<version>\\S+)'],
+        depNameTemplate: 'helm/ttc-standard-app',
+        registryUrlTemplate: 'https://tcxcontainers.azurecr.io',
+        versioningTemplate: 'semver-coerced',
+        datasourceTemplate: 'docker',
+        currentValueTemplate: '{{version}}'
+      }
+    ],
+    packageRules: [
+      {
+        matchDatasources: ['docker'],
+        matchPackageNames: ['helm/ttc-standard-app'],
+        versioning: 'semver-coerced'
+      }
+    ],
+    hostRules: [
+      {
+        matchHost: 'https://tcxcontainers.azurecr.io',
+        username: 'tcxcontainers',
+        password: '1234'
+      }
+    ]
+  };
+  
